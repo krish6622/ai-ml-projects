a21153cd3138644b44217be73ce811063c1c7208
ayyapps <ayyapssoft@gmail.com>
2025-08-29 19:39:48 +0530

    SVM scratch

---
+++ Support_Vector_Machine/svm_scratch.py
@@ -0,0 +1,121 @@
+import numpy as np
+from sklearn import datasets
+import matplotlib.pyplot as plt
+import imageio  # for GIF
+
+class SVM:
+    def __init__(self, learning_rate=0.001, lambda_param=0.01, n_iters=1000):
+        self.lr = learning_rate
+        self.lambda_param = lambda_param
+        self.n_iters = n_iters
+        self.w = None
+        self.b = None
+
+    def fit(self, X, y, save_frames=False):
+        n_samples, n_features = X.shape
+        y_ = np.where(y <= 0, -1, 1)
+        self.w = np.zeros(n_features)
+        self.b = 0
+
+        frames = []  # store filenames if save_frames=True
+
+        for iteration in range(self.n_iters):
+            for idx, x_i in enumerate(X):
+                condition = y_[idx] * (np.dot(x_i, self.w) - self.b) >= 1
+                if condition:
+                    self.w -= self.lr * (2 * self.lambda_param * self.w)
+                else:
+                    self.w -= self.lr * (2 * self.lambda_param * self.w - np.dot(x_i, y_[idx]))
+                    self.b -= self.lr * y_[idx]
+
+            # save frame every 10 iterations
+            if save_frames and iteration % 10 == 0:
+                filename = f"frame_{iteration}.png"
+                self._save_frame(X, y, filename)
+                frames.append(filename)
+
+        if save_frames:
+            # create GIF
+            with imageio.get_writer('svm_training.gif', mode='I', duration=0.5) as writer:
+                for filename in frames:
+                    image = imageio.imread(filename)
+                    writer.append_data(image)
+            print("GIF saved as svm_training.gif")
+
+    def predict(self, X):
+        approx = np.dot(X, self.w) - self.b
+        return np.sign(approx)
+
+    def _save_frame(self, X, y, filename):
+        def get_hyperplane_value(x, w, b, offset):
+            return (-w[0]*x + b + offset)/w[1]
+
+        fig, ax = plt.subplots()
+        ax.scatter(X[:,0], X[:,1], c=y, cmap='bwr')
+
+        x0_1 = np.amin(X[:,0])
+        x0_2 = np.amax(X[:,0])
+
+        x1_1 = get_hyperplane_value(x0_1, self.w, self.b, 0)
+        x1_2 = get_hyperplane_value(x0_2, self.w, self.b, 0)
+
+        x1_1_m = get_hyperplane_value(x0_1, self.w, self.b, -1)
+        x1_2_m = get_hyperplane_value(x0_2, self.w, self.b, -1)
+
+        x1_1_p = get_hyperplane_value(x0_1, self.w, self.b, 1)
+        x1_2_p = get_hyperplane_value(x0_2, self.w, self.b, 1)
+
+        ax.plot([x0_1, x0_2], [x1_1, x1_2], 'y--')
+        ax.plot([x0_1, x0_2], [x1_1_m, x1_2_m], 'k')
+        ax.plot([x0_1, x0_2], [x1_1_p, x1_2_p], 'k')
+
+        x1_min = np.amin(X[:,1])
+        x1_max = np.amax(X[:,1])
+        ax.set_ylim([x1_min - 3, x1_max + 3])
+
+        plt.savefig(filename)
+        plt.close(fig)
+
+
+# Testing
+if __name__ == "__main__":
+    X, y = datasets.make_blobs(n_samples=50, n_features=2, centers=2, cluster_std=1.05, random_state=40)
+    y = np.where(y == 0, -1, 1)
+
+    clf = SVM()
+    clf.fit(X, y, save_frames=True)
+
+    print("Weights:", clf.w)
+    print("Bias:", clf.b)
+
+    # Final visualization
+    def visualize_svm():
+        def get_hyperplane_value(x, w, b, offset):
+            return (-w[0]*x + b + offset)/w[1]
+
+        fig, ax = plt.subplots()
+        ax.scatter(X[:,0], X[:,1], c=y, cmap='bwr')
+
+        x0_1 = np.amin(X[:,0])
+        x0_2 = np.amax(X[:,0])
+
+        x1_1 = get_hyperplane_value(x0_1, clf.w, clf.b, 0)
+        x1_2 = get_hyperplane_value(x0_2, clf.w, clf.b, 0)
+
+        x1_1_m = get_hyperplane_value(x0_1, clf.w, clf.b, -1)
+        x1_2_m = get_hyperplane_value(x0_2, clf.w, clf.b, -1)
+
+        x1_1_p = get_hyperplane_value(x0_1, clf.w, clf.b, 1)
+        x1_2_p = get_hyperplane_value(x0_2, clf.w, clf.b, 1)
+
+        ax.plot([x0_1, x0_2], [x1_1, x1_2], 'y--')
+        ax.plot([x0_1, x0_2], [x1_1_m, x1_2_m], 'k')
+        ax.plot([x0_1, x0_2], [x1_1_p, x1_2_p], 'k')
+
+        x1_min = np.amin(X[:,1])
+        x1_max = np.amax(X[:,1])
+        ax.set_ylim([x1_min - 3, x1_max + 3])
+
+        plt.show()
+
+    visualize_svm()
